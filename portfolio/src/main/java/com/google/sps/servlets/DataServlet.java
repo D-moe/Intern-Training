69338c4e3e0e7c1c17f5deecebaf499df32e6878
morleyd <morleyd@penguin>
2020-06-26 12:55:07 -0700

    Minor adjustements for part 3

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps.servlets;
 
 import java.io.IOException;
@@ -33,4 +19,28 @@ public class DataServlet extends HttpServlet {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello David Morley!</h1>");
   }
+  /*
+   * We now need to modify this file to properly handle JSON
+   * This shouldn't be too difficult
+   */
 }
+
+@WebServlet("/json")
+public class JsonServlet extends HttpServlet {
+  private ArrayList<String> data = new ArrayList<>(Arrays.asList(
+      "Nice shirt", "that's interesting", "what a crappy website"));
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/html;");
+    Gson gson = new Gson();
+    String json = gson.toJson(data);
+    response.getWriter().println(json);
+  }
+  /*
+   * We now need to modify this file to properly handle JSON
+   * This shouldn't be too difficult
+   */
+}
+
