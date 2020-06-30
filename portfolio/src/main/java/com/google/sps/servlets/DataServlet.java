e76f3ae9f41437182f4f32b8ef3312af12e1f9a7
morleyd <morleyd@penguin>
2020-06-30 14:53:53 -0700

    Add comment functionality. New styling changes for comments. Sketch out new botom layout.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.sps.objects.User;
 /**
  * Servlet that returns some example content. TODO(morleyd): modify this file
  * to handle comments data
@@ -17,15 +19,33 @@ import com.google.gson.Gson;
 
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private ArrayList<String> data = new ArrayList<>(Arrays.asList(
-      "Nice shirt", "that is interesting", "what a crappy website"));
+  private ArrayList<User> userData = new ArrayList<User>();
 
+  // Intent is for the get method to be called on the load of the comment body.
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
     response.setContentType("text/html;");
-    Gson gson = new Gson();
-    String json = gson.toJson(data);
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    String json = gson.toJson(userData);
+    System.out.format("Printting out the following JSON: %s", json);
     response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/html");
+    // The additional parameters used here ensure that the generated JSON looks
+    // much nicer to parse in the browser.
+    String userName = request.getParameter("user");
+    String comment = request.getParameter("comment-body");
+    userData.add(new User(userName, comment));
+    System.out.format("The username is %s\n. The comment body is %s:", userName,
+                      comment);
+    response.sendRedirect("/index.html");
+    // Handle possible scripting attacks on the JavaScript side when we receive
+    // the response. Should consider whether or not this issue should be handled
+    // on the server side or not.
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,7 +14,9 @@
 
 // random image generator
 window.onload = SetImageOnMouseOver;
-
+window.onload = (event) => {
+  loadComments();
+};
 /**
  * Change the given image to a random one whenever the user hovers over
  */
@@ -60,20 +62,80 @@ function fetchAndModify(path, element) {
       .then(response => response.text())
       .then(content => element.innerHTML = content)
 }
+/*
+ * This code creates an innerHTML and then reads its text to remove special
+ * characters from the input
+ * @param {string} str The input to remove special characters from to prevent
+ *     scripting attacks
+ */
+var sanitizeHTML = function(str) {
+  var temp = document.createElement('div');
+  temp.textContent = str;
+  return temp.innerHTML;
+};
+
+
+/*
+ * This function fetches the value return by the given data service and then
+ * modifies the content of a div to store each of the response values as a
+ * paragraph.
+ * @param {string} path The url of the data service to use
+ */
 function fetchAndChangeBody(path) {
-  fetch(path)
+  fetch(path).then(response => response.json()).then(json => {
+    let index = 0;
+    for (index in json) {
+      console.log(json[index]);
+      const target = document.getElementById('comment');
+      let newComment = document.createElement('p');
+      newComment.innerText = json[index];
+      target.appendChild(newComment);
+      index++;
+    }
+  })
+}
+
+/*
+ * This function fetches the comment data and adds each of them to the loaded
+ * content.
+ * TODO(morleyd): change individual comment load to be asynchronous to improve
+ * performance. It might also make sense to have a hard limit on the number of
+ * comments loaded, think about this for future implementations.
+ */
+function loadComments() {
+  fetch('\data')
       .then(response => response.json())
       .then(
           json => {
+            console.log(json)
             var index = 0;
             for (index in json) {
-              console.log(json[index]);
-              const target = document.getElementById("comment");
-              let newComment = document.createElement("p");
-              newComment.innerText = json[index];
-              target.appendChild(newComment);
-              index++;
+              let userName = json[index]['userName'];
+              console.log(userName)
+              let commentBody = json[index]['commentBody'];
+              addComment(/* name= */ userName, /* commentBody= */ commentBody);
             }
           }
+
       )
+}
+
+/**
+ * A helper that creates the divs for a new comment, intended to be called by
+ * loadComments.
+ * @param {string} name The name of the user who made the comment
+ * @param {string} content The body of the comment
+ */
+function addComment(name, content) {
+  const target = document.getElementById('prior-comments');
+  let newComment = document.createElement('div');
+  let body = newComment.appendChild(document.createElement('div'));
+  body.className = 'comment-body'
+  let user = newComment.appendChild(document.createElement('div'));
+  user.className = 'sub-script'
+  // We need to make sure to strip comments of any special chars they may have
+  // in order to prevent injection attacks
+  user.innerText = '-' + sanitizeHTML(name);
+  body.innerText = sanitizeHTML(content);
+  target.appendChild(newComment);
 }
