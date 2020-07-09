225c2fa261f43434dfe88f73517eda25e3d10de8
morleyd <morleyd@penguin>
2020-07-09 11:27:08 -0700

    Setting up initial class structure, login and logout buttons.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -2,6 +2,7 @@ package com.google.sps.servlets;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
---
+++ portfolio/src/main/java/com/google/sps/servlets/LoginInfo.java
@@ -0,0 +1,60 @@
+package com.google.sps.servlets;
+
+import java.util.Objects;
+
+public class LoginInfo {
+  private boolean loggedIn;
+  private String userName;
+
+  public LoginInfo() {}
+
+  public LoginInfo(boolean loggedIn, String userName) {
+    this.loggedIn = loggedIn;
+    this.userName = userName;
+  }
+
+  public boolean isLoggedIn() { return this.loggedIn; }
+
+  public boolean getLoggedIn() { return this.loggedIn; }
+
+  public void setLoggedIn(boolean loggedIn) { this.loggedIn = loggedIn; }
+
+  public String getUserName() { return this.userName; }
+
+  public void setUserName(String userName) { this.userName = userName; }
+
+  public LoginInfo loggedIn(boolean loggedIn) {
+    this.loggedIn = loggedIn;
+    return this;
+  }
+
+  public LoginInfo userName(String userName) {
+    this.userName = userName;
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this)
+      return true;
+    if (!(o instanceof LoginInfo)) {
+      return false;
+    }
+    LoginInfo loginInfo = (LoginInfo)o;
+    return loggedIn == loginInfo.loggedIn &&
+        Objects.equals(userName, loginInfo.userName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(loggedIn, userName);
+  }
+
+  @Override
+  public String toString() {
+    return "{"
+        + " loggedIn='" + isLoggedIn() + "'"
+        + ", userName='" + getUserName() + "'"
+        + "}";
+  }
+}
