d3a8f8b29dbb62cadb348f50b4beeaf154ee188f
morleyd <morleyd@penguin>
2020-07-13 10:03:16 -0700

    Reduce bloat in LoginInfo class, simplify to just getters, setters, and constructor

--- portfolio/src/main/java/com/google/sps/servlets/LoginInfo.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginInfo.java
@@ -19,8 +19,6 @@ public class LoginInfo {
     this.userName = userName;
   }
 
-  public boolean isLoggedIn() { return this.loggedIn; }
-
   public boolean getLoggedIn() { return this.loggedIn; }
 
   public void setLoggedIn(boolean loggedIn) { this.loggedIn = loggedIn; }
@@ -28,39 +26,4 @@ public class LoginInfo {
   public String getUserName() { return this.userName; }
 
   public void setUserName(String userName) { this.userName = userName; }
-
-  public LoginInfo loggedIn(boolean loggedIn) {
-    this.loggedIn = loggedIn;
-    return this;
-  }
-
-  public LoginInfo userName(String userName) {
-    this.userName = userName;
-    return this;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == this)
-      return true;
-    if (!(o instanceof LoginInfo)) {
-      return false;
-    }
-    LoginInfo loginInfo = (LoginInfo)o;
-    return loggedIn == loginInfo.loggedIn &&
-        Objects.equals(userName, loginInfo.userName);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(loggedIn, userName);
-  }
-
-  @Override
-  public String toString() {
-    return "{"
-        + " loggedIn='" + isLoggedIn() + "'"
-        + ", userName='" + getUserName() + "'"
-        + "}";
-  }
 }
